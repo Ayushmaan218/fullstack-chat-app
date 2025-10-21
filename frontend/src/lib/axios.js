2b734a7ddb9af818791521fa44c7393efec8c7c4
Ayushmaan218 <121386512+Ayushmaan218@users.noreply.github.com>
2025-10-21 10:44:55 +0530

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
+  baseURL
       : "https://fullstack-chat-app-backend-three.vercel.app/api",
   withCredentials: true,
 });
