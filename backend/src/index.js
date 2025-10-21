53105212390e8843541661e1d940354a9eaac182
Ayushmaan218 <121386512+Ayushmaan218@users.noreply.github.com>
2025-10-21 11:03:46 +0530

    complete project

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http:https://fullstack-chat-app-sage-tau.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL
-      : "https://fullstack-chat-app-backend-three.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
